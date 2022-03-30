0037a4e699d39129a72f39a13977cc8ba3851000
Marija Klještan <marijakljestan@uns.ac.rs>
2022-03-30 14:48:12 +0200

    feat: Show rented cars to user

--- front/src/app/home-page/home-page.component.ts
+++ front/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehicle } from '../model/vehicle.model';
 
 @Component({
   selector: 'app-home-page',
@@ -6,13 +7,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
+  selectedVehicle: Vehicle = {
+    model: "Tesla serie 4",
+    description: "Electric car",
+    pricePerDay: 500,
+    garage: {
+        address: {
+          streetName: "Trifkovicev trg",
+          streetNumber: "6",
+          city: "Novi Sad"
+        }
+    },
+    images: ["../../assets/images/tesla2.jpg"]
+  };
+  availableVehicles: Vehicle[] = [
+    {
+        model: "Tesla serie 4",
+        description: "Electric car",
+        pricePerDay: 500,
+        garage: {
+            address: {
+              streetName: "Trifkovicev trg",
+              streetNumber: "6",
+              city: "Novi Sad"
+            }
+        },
+        images: ["../../assets/images/tesla2.jpg"]
+    },
+    {
+        model: "Tesla serie 3",
+        description: "Electric car",
+        pricePerDay: 450,
+        garage: {
+            address: {
+              streetName: "Hadzi Ruvimova",
+              streetNumber: "8",
+              city: "Beograd"
+            }
+        },
+        images: ["../../assets/images/tesla2.jpg"]
+    },
+  ];
+  rentedVehicles: Vehicle[] = [
+    {
+        model: "Tesla serie 3",
+        description: "Electric car",
+        pricePerDay: 450,
+        garage: {
+            address: {
+              streetName: "Hadzi Ruvimova",
+              streetNumber: "8",
+              city: "Beograd"
+            }
+        },
+        images: ["../../assets/images/tesla2.jpg"],
+        rentedPeriodInDays: 2
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
 
-  showCarDetailsModal(event: MouseEvent): void {
+  showCarDetailsModal(event: MouseEvent, vehicle: Vehicle): void {
     event.preventDefault();
+    this.selectedVehicle = vehicle;
     (document.querySelector('#vehicle-details-modal') as HTMLElement).style.display = 'flex';
   }
 
