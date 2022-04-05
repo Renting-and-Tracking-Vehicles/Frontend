f383a1d5c2206cbe4a5c46cf59d927d971462baf
NikolinaPavkovic <73259509+NikolinaPavkovic@users.noreply.github.com>
2022-04-05 14:42:21 +0200

    feat: aws deployment (#12)

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlVehicle: 'http://localhost:1025/vehicle/',
+  baseUrlRenting: 'http://localhost:1025/renting/',
+  baseUrlGarage:  'http://localhost:1025/garage/',
+  baseUrlUser:    'http://localhost:1026/users/',
+  baseUrlAuth:    'http://localhost:1026/auth/'
 };
